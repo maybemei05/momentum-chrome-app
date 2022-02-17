66fe9424a7b732bc7c3c6b7ce7c91192365ee782
May <eucho05@gmail.com>
2022-02-17 21:34:27 +0900

    Add quotes and background

---
+++ js/background.js
@@ -0,0 +1,27 @@
+const images = [
+  "1.jpg",
+  "2.jpg",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png",
+  "7.png",
+  "8.png",
+  "9.png",
+  "10.png",
+  "11.png",
+  "12.png",
+  "13.png",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+]
+
+const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+
--- js/clock.js
+++ js/clock.js
@@ -11,5 +11,5 @@ function getClock() {
 // setInterval(sayHello(), 5000); 5초 후에 sayHello() 함수 실행, 이후 5초 마다 반복
 // setTimeout(sayHello(), 5000); 5초 후에 sayHello() 함수 실행, 반복 없음
 
-getClock() // 웹사이트가 로드 됐을 때 바로 보여주도록 호출, 아래는 1초 후부터 나타남
+getClock(); // 웹사이트가 로드 됐을 때 바로 보여주도록 호출, 아래는 1초 후부터 나타남
 setInterval(getClock, 1000); //1초마다 갱신해줌 -> 1초마다 초가 바뀜
