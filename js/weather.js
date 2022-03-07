bbd86b4fe7f0a804eafad3373ecddcee7491b39b
May <eucho05@gmail.com>
2022-03-08 01:20:52 +0900

    Update weather

--- js/weather.js
+++ js/weather.js
@@ -3,17 +3,23 @@ const API_KEY = "64ae8da12abee27ca8b16fbd19c00578";
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
-`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
-  .then(response => response.json())
-  .then((data) => {
-    const weather = document.querySelector("#weather span:first-child");
-    const city = document.querySelector("#weather span:last-child");
-    city.innerText = data.name;
-    weather.innerText = `${data.weather[0].main}, ${data.main.temp}°C` ;
-
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `📍 ${data.name}`;
+      const weatherNow = data.weather[0].main;
+      if (weatherNow === "Clouds") {
+        const weatherNowEmoji = "⛅";
+        weather.innerText = `${weatherNowEmoji} ${data.main.temp}°C`;
+      } else if (weatherNow === "Clear") {
+        const weatherNowEmoji = "☀️";
+        weather.innerText = `${weatherNowEmoji} ${data.main.temp}°C`;
+      }
+      console.log(url);
+    });
 }
 
 function onGeoError() {
@@ -21,4 +27,4 @@ function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError)
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
