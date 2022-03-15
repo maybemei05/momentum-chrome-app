d4ce123f1ab511b1c8798f620ea8b6edee7432ae
May <eucho05@gmail.com>
2022-03-15 20:46:42 +0900

    Add youtube playlist

--- js/greeting.js
+++ js/greeting.js
@@ -5,10 +5,10 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "usename";
 
-function onLoginSubmit(event){
+function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기
   loginForm.classList.add(HIDDEN_CLASSNAME); //폼을 제출했을 때 login-form 클래스에 hidden 클래스명 추가하기
-  const username = loginInput.value;//폼에 이름을 입력했을 때의 value를 가져옴
+  const username = loginInput.value; //폼에 이름을 입력했을 때의 value를 가져옴
   // greeting.innerText = "Hello " + username;//h1태그에 유저네임을 넣는다.
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -19,11 +19,13 @@ function paintGreetings(username) {
   greeting.classList.remove(HIDDEN_CLASSNAME); //greeting의 hidden을 삭제 하여 보여지게 함
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);//유저 정보 확인
+const savedUsername = localStorage.getItem(USERNAME_KEY); //유저 정보 확인
 
-if (savedUsername === null){//유저 네임이 없을 때
-  loginForm.classList.remove(HIDDEN_CLASSNAME);//form의 hidden을 삭제 하여 보여지게 함
-  loginForm.addEventListener("submit", onLoginSubmit);//보여진 form에 내용을 입력(submit)하면 함수 실행
-} else { //유저 네임이 있을 때
+if (savedUsername === null) {
+  //유저 네임이 없을 때
+  loginForm.classList.remove(HIDDEN_CLASSNAME); //form의 hidden을 삭제 하여 보여지게 함
+  loginForm.addEventListener("submit", onLoginSubmit); //보여진 form에 내용을 입력(submit)하면 함수 실행
+} else {
+  //유저 네임이 있을 때
   paintGreetings(savedUsername);
 }
--- js/playlist.js
+++ js/playlist.js
@@ -1,3 +1,35 @@
+const playlistForm = document.querySelector("#playlist-form");
+const playlistInput = playlistForm.querySelector("#playlist-form input");
+const youtubePlayer = document.querySelector("#player");
+
+const PLAYLIST_KEY = "playlistCode";
+
+function onPlaylistCodeSubmit(event) {
+  event.preventDefault();
+  const playlistCode = playlistInput.value;
+  localStorage.setItem(PLAYLIST_KEY, playlistCode);
+  paintPlaylist();
+}
+
+function paintPlaylist() {
+  youtubePlayer.classList.remove(HIDDEN_CLASSNAME); //youtubePlayer hidden을 삭제 하여 보여지게 함
+}
+
+const savedPlaylistCode = localStorage.getItem(PLAYLIST_KEY); //유저 정보 확인
+
+console.log(savedPlaylistCode);
+
+if (savedPlaylistCode === null) {
+  //유저 네임이 없을 때
+  playlistForm.classList.remove(HIDDEN_CLASSNAME); //form의 hidden을 삭제 하여 보여지게 함
+  playlistForm.addEventListener("submit", onPlaylistCodeSubmit); //보여진 form에 내용을 입력(submit)하면 함수 실행
+} else {
+  //유저 네임이 있을 때
+  paintPlaylist(savedPlaylistCode);
+}
+
+console.log(savedPlaylistCode);
+
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement("script");
 
@@ -8,13 +40,14 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
-function onYouTubePlayerAPIReady() {
+
+function onYouTubePlayerAPIReady(playlistCode) {
   player = new YT.Player("player", {
-    // height: "550",
-    width: "300",
+    // height: "300",
+    // width: "200",
     playerVars: {
       listType: "playlist",
-      list: "PLPxxYwfAvXO-lzkrJeTGPyqitAiiXrTNJ",
+      list: playlistCode,
     },
   });
 }
@@ -30,7 +63,7 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
+    setTimeout(stopVideo, 1000);
     done = true;
   }
 }
